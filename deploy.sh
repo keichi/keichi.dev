b3f19fded7dcb8b48840575e621563d2014ecd4f
Keichi Takahashi <keichi.t@me.com>
2015-02-05 01:10:52 +0900

    デプロイ用のスクリプトを追加

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+hugo -t vienna
+cd public
+msg="Update on `date '+%Y/%m/%d %H:%M:%S'`"
+if [ $# -eq 1 ]
+    then msg="$1"
+fi
+git add -A
+git commit -m "$msg"
+git push origin master
+
