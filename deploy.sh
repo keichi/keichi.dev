f407cf92415ad57afaac4f36fc13203593d25ddb
Keichi Takahashi <keichi.t@me.com>
2017-09-20 15:16:47 +0900

    Deploy to S3

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
 hugo
-cd public
-msg="Update on `date '+%Y/%m/%d %H:%M:%S'`"
-if [ $# -eq 1 ]
-    then msg="$1"
-fi
-git add -A
-git commit -m "$msg"
-git push origin master
-
+aws s3 sync public/ s3://blog.keichi.net --delete --exclude ".DS_Store"
